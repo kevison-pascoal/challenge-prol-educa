4de4f1f54d0723a66d016a2f6e93c00902596b46
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-06-28 14:49:07 -0300

    updated serverPhp:zap:

--- server.php
+++ server.php
@@ -1,14 +1,19 @@
 <?php
   include('./class/connect.php');
   $name = $_POST['name'];
+  $telephone = $_POST['telephone'];
   $email = $_POST['email'];
-  $password = $_POST['password'];
+  $date = $_POST['date'];
+  $mother = $_POST['mother'];
+  $father = $_POST['father'];
+  $state = $_POST['state'];
+  $city = $_POST['city'];
   $id = rand(1000, 9999);
   if($name == null or $email == null or $password == null) {
     header('location: index.php');  
     die("<br>Error! Fill in all fields");
   } 
-  mysqli_query($orm, "INSERT INTO register VALUES ('$id', '$name', '$email', '$password')") or die("<br>Acess Denied.");
+  mysqli_query($orm, "INSERT INTO register VALUES ('$id', '$name', '$telephone', '$email', '$date', '$mother', '$father', '$state', '$city')") or die("<br>Acess Denied.");
   while($row = mysqli_fetch_object($consult)) {
     if($row -> name == null or $row -> email == null or $row -> password == null) {
       mysqli_query($orm, "DELETE FROM register WHERE id=$row->id");
