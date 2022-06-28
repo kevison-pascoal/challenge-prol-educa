828186c0e4cbf860047e834cf376504dba959eea
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-06-28 15:12:42 -0300

    updated indexPhp:zap:

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+<!--
 <?php 
   include('./class/connect.php');
 ?>
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +10,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./css/app.css">
+  <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
   <title>Challenge</title>
 </head>
 <body>
   <section id="app">
     <h1 class="titleSection">Challenge - Prol Educa</h1>
-    <input type="button" id="list" onclick="listData()" value="List">
-    <input type="button" id="register" onclick="toRegister()" value="Register">
+    <input type="button" id="list" class="btn btn-outline-primary" onclick="listData()" value="List"></input>
+    <input type="button" id="register" class="btn btn-outline-primary" onclick="toRegister()" value="Register"></input>
   </section>
   <script src="./js/app.js"></script>
   <script type="text/javascript">
