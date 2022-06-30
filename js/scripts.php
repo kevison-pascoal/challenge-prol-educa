898326c8a7fcf2a4afd2a023cd9524dd84c526ec
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-06-30 14:49:51 -0300

    updated scripts:zap:

--- js/scripts.php
+++ js/scripts.php
@@ -26,32 +26,6 @@
           <div class="input-group">
             <select class="custom-select" name="state" id="inputGroupSelect02">
               <option selected>Choose your state...</option>
-              <option value="Acre - (AC)">Acre - (AC)</option>
-              <option value="Alagoas - (AL)">Alagoas - (AL)</option>
-              <option value="Amapá - (AP)">Amapá - (AP)</option>
-              <option value="Bahia - (BA)">Bahia - (BA)</option>
-              <option value="Ceará - (CE)">Ceará - (CE)</option>
-              <option value="Distrito Federal - (DF)">Distrito Federal - (DF)</option>
-              <option value="Espírito Santo - (ES)">Espírito Santo - (ES)</option>
-              <option value="Goiás - (GO)">Goiás - (GO)</option>
-              <option value="Maranhão - (MA)">Maranhão - (MA)</option>
-              <option value="Mato Grosso - (MT)">Mato Grosso - (MT)</option>
-              <option value="Mato Grosso do Sul - (MS)">Mato Grosso do Sul - (MS)</option>
-              <option value="Minas Gerais (MG)">Minas Gerais (MG)</option>
-              <option value="Pará - (PA)">Pará - (PA)</option>
-              <option value="Paraíba - (PB)">Paraíba - (PB)</option>
-              <option value="Paraná - (PR)">Paraná - (PR)</option>
-              <option value="Pernambuco - (PE)">Pernambuco - (PE)</option>
-              <option value="Piauí - (PI)">Piauí - (PI)</option>
-              <option value="Rio de Janeiro - (RJ)">Rio de Janeiro - (RJ)</option>
-              <option value="Rio Grande do Norte - (RN)">Rio Grande do Norte - (RN)</option>
-              <option value="Rio Grande do Sul - (RS)">Rio Grande do Sul - (RS)</option>
-              <option value="Rondônia - (RO)">Rondônia - (RO)</option>
-              <option value="Roraima - (RR)">Roraima - (RR)</option>
-              <option value="Santa Catarina - (SC)">Santa Catarina - (SC)</option>
-              <option value="São Paulo - (SP)">São Paulo - (SP)</option>
-              <option value="Sergipe - (SE)">Sergipe - (SE)</option>
-              <option value="Tocantins - (TO)">Tocantins - (TO)</option>
             </select>
             <input type="text" name="city" id="city" class="form-control" placeholder="write your city" aria-describedby="basic-addon1">
           </div>
@@ -62,6 +36,13 @@
         </div>
       </form>
     `   
+    genereteStates();
+  }
+  function genereteStates() {
+    const element = document.getElementById('inputGroupSelect02');
+    for(let index = 0; index < states.length; index++) { 
+      element.innerHTML += `<option value="${states[index]}">${states[index]}</option>`;
+    }
   }
   function listData() {
     app.innerHTML = `
@@ -89,7 +70,7 @@
               ";
             }   
           ?>
-        </tbody>  
+        </tbody>
       </table>
       <input type="button" id="back" class="btn btn-outline-secondary btn-lg" onclick="backToIndex()" value="Back" style="margin: 2rem; color: #FAFAFA"></input>
     `
